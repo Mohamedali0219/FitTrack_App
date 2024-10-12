ab6175341ef7bf1a3ed48ed6840d3bbba2076ceb
youssef883 <79144849+youssef883@users.noreply.github.com>
2024-10-12 15:41:49 +0300

    update: app_settings_screen

---
+++ lib/features/ap_settings/ui/widget/settings_app_items/settings_app_items.dart
@@ -0,0 +1,26 @@
+import 'package:fit_track_app/core/themes/colors_manager.dart';
+import 'package:flutter/material.dart';
+
+class SettingsAppItems extends StatelessWidget {
+  const SettingsAppItems({super.key, required this.icon, required this.title, required this.training});
+  final IconData icon;
+  final String title;
+  final Widget training;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: (){},
+      leading: Icon(icon, size: 20, color: ColorsManager.blackColor,),
+      title: Text(title),
+      titleTextStyle: const TextStyle(
+        color: ColorsManager.textBaseColor,
+        fontSize: 14,
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w500,
+      ),
+      shape: const Border(bottom: BorderSide(color: Color(0xFFE9ECEF), width: 1),),
+      trailing: training,
+    );
+  }
+}
