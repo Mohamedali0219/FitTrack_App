850596e0308573197edce7a627738e99bbe7de01
Meroothman <011199230a@gmail.com>
2024-10-16 01:14:54 +0300

    edit toggleButton

--- lib/features/auth/ui/widgets/number_input.dart
+++ lib/features/auth/ui/widgets/number_input.dart
@@ -42,26 +42,63 @@ class _NumberInputWithUnitState extends State<NumberInputWithUnit> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(widget.title, style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+        const SizedBox(height: 20),
+        Text(widget.title,
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
         const SizedBox(height: 10),
         ToggleButtons(
-          isSelected: [selectedUnit == widget.unit1, selectedUnit == widget.unit2],
+          fillColor: Colors.grey,
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          color: Colors.grey,
+          selectedColor: Colors.white,
+          isSelected: [
+            selectedUnit == widget.unit1,
+            selectedUnit == widget.unit2
+          ],
           onPressed: (index) {
             setState(() {
               selectedUnit = index == 0 ? widget.unit1 : widget.unit2;
             });
           },
-          children: [Text(widget.unit1), Text(widget.unit2)],
+          children: [
+            Text(widget.unit1,
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: selectedUnit == widget.unit1
+                        ? Colors.black
+                        : Colors.grey)),
+            Text(widget.unit2,
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: selectedUnit == widget.unit2
+                        ? Colors.black
+                        : Colors.grey)),
+          ],
         ),
-        const SizedBox(height: 20),
+        const SizedBox(height: 40),
         TextField(
           controller: controller,
           keyboardType: TextInputType.number,
-          onChanged: (value) => widget.onValueChange(value),
+          onChanged: (value) {
+            widget.onValueChange(value);
+            controller.text.isNotEmpty && selectedUnit == widget.unit2
+                ? (double.tryParse(controller.text)! * 2.205).toStringAsFixed(0)
+                : '0';
+          },
           decoration: InputDecoration(
             suffixText: selectedUnit,
-            border: const OutlineInputBorder(),
-            contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            border: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              borderSide: BorderSide(color: Colors.grey[800]!),
+            ),
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
           ),
         ),
         const SizedBox(height: 20),
